cea043a934a169bd8f3458f554545c88c3b4453d
antoniorafael <rafasystec@yahoo.com.br>
2017-05-05 13:40:14 -0300

    Novas implementações para o Spring Security.

---
+++ Petshow-WEB/src/main/java/br/com/petshow/security/GrantedAuthorityImpl.java
@@ -0,0 +1,16 @@
+package br.com.petshow.security;
+
+import org.springframework.security.core.GrantedAuthority;
+
+public class GrantedAuthorityImpl implements GrantedAuthority {
+
+	public GrantedAuthorityImpl(String role) {
+		// TODO Auto-generated constructor stub
+	}
+	@Override
+	public String getAuthority() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
