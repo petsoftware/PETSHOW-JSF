139c428847578020ce7b8f4fadf67493a8d0d815
rafasystec <rafasystec@yahoo.com.br>
2017-10-15 10:51:26 -0200

    Chamada do count perfil de adoção.

--- Petshow-WEB/src/main/java/br/com/petshow/web/util/CallPerfilAdocaoRest.java
+++ Petshow-WEB/src/main/java/br/com/petshow/web/util/CallPerfilAdocaoRest.java
@@ -1,10 +1,17 @@
 package br.com.petshow.web.util;
 
+import java.util.List;
+
+import org.jboss.resteasy.client.jaxrs.ResteasyClientBuilder;
+
 import br.com.petshow.constants.RestPathConstants;
+import br.com.petshow.enums.EnumErrosSistema;
 import br.com.petshow.exceptions.ExceptionErroCallRest;
 import br.com.petshow.exceptions.ExceptionValidation;
+import br.com.petshow.model.Adocao;
 import br.com.petshow.model.PerfilAdocao;
 import br.com.petshow.model.Usuario;
+import br.com.petshow.util.MapErroRetornoRest;
 
 public class CallPerfilAdocaoRest extends RestUtilCall<PerfilAdocao> {
 	
@@ -27,6 +34,17 @@ public class CallPerfilAdocaoRest extends RestUtilCall<PerfilAdocao> {
 		} catch (ExceptionErroCallRest | ExceptionValidation e) {
 			throw e;
 		}
+	}
+	public static long getCountAnimalDisponiveisAdocaoPorPerfil(Usuario usuario) throws ExceptionErroCallRest, ExceptionValidation{
+		client = new ResteasyClientBuilder().build();
+		target= client.target(URL_BASE+RestPathConstants.PATH_PERFIL_ADOCAO+RestPathConstants.GET+"/count/"+usuario.getId());
+		long count = 0;
+		try{
+			count =  target.request().get(Long.class);
+		}catch(Exception ex){
+			throw new ExceptionErroCallRest("Failed: HTTP error code:"+ex.getMessage());
+		}
+		return count;
 	}
 	
 }
